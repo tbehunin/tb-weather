2a4e499e6e3057a181519d580b13b6f5c7271d23
Todd Behunin <tbehunin@cvent.com>
2020-04-28 14:20:06 -0700

    create search component

---
+++ src/components/Search.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import api from '../utils/api';
+import './Search.css';
+
+function Search({ onSearchResults }) {
+  const [weatherData, setWeatherData] = useState();
+  const [searching, setSearching] = useState(false);
+  const [debouncedCallback] = useDebouncedCallback(async (value) => {
+    setSearching(true);
+    const data = await (value.trim().length > 0 ? api.search(value) : Promise.resolve());
+    setSearching(false);
+    setWeatherData(data);
+  }, 800);
+  const found = (weatherData || {}).cod === 200;
+  const notFound = (weatherData || {}).cod === "404";
+  
+  return (
+    <div>
+      <InputGroup>
+        <FormControl
+          id="cityName"
+          laceholder="Enter location name"
+          maxLength="25"
+          isInvalid={notFound && !searching}
+          onChange={e => debouncedCallback(e.target.value)}
+        />
+        {(searching || found || notFound) &&
+          <InputGroup.Append>
+            {searching &&
+              <InputGroup.Text>
+                <i className="fas fa-spinner fa-spin"></i>
+                <span className="searching-text">Searching</span>
+              </InputGroup.Text>
+            }
+            {found && !searching &&
+              <Button onClick={() => onSearchResults(weatherData)}>Found:
+                <span className="found-location-text">{weatherData.name}</span>
+                <i className="fas fa-arrow-right"></i>
+              </Button>
+            }
+            {notFound && !searching &&
+              <InputGroup.Text className="not-found">Location not found</InputGroup.Text>
+            }
+          </InputGroup.Append>
+        }
+      </InputGroup>
+    </div>
+  );
+}
+
+export default Search;
