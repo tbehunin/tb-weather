451ea03b83c99b11bee080c9119a6fa185b66bdb
Todd Behunin <tbehunin@cvent.com>
2020-04-29 12:44:15 -0700

    layout tweaks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <Container fluid>
       <Home />
-    </div>
+    </Container>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import { Row } from 'react-bootstrap';
 import Search from '../components/Search';
 import WeatherData from '../components/WeatherData';
+import './Home.css';
 
 function Home() {
   const [weatherData, setWeatherData] = useState();
   console.log('Home', weatherData);
   return (
-    <div>
-      <Search onSearchResults={data => setWeatherData(data)} />
-      <WeatherData data={weatherData} />
-    </div>
+    <Fragment>
+      <Row className="justify-content-center">
+        <Search onSearchResults={data => setWeatherData(data)} />
+      </Row>
+      <Row className="justify-content-center">
+        <WeatherData data={weatherData} />
+      </Row>
+    </Fragment>
   );
 }
 
